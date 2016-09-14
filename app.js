515ba0353dfd5f70d576d52e59f0ee459e1864f4
kmjacobsen <kelsey.jacobsen@aol.com>
2016-09-14 09:28:54 -0700

    modified javascript file

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
+  $('.formInput').on('submit', function(event) {
+    event.preventDefault();
 
-}); //paris with document ready function
+    var inputNumber = $('.inputBox').val();
+    var number = +inputNumber;
 
-for (var i = 1; i < 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log('fizzbuzz');
-  }
-  else if (i % 5 === 0) {
-    console.log('buzz');
-  }
-  else if (i % 3 === 0) {
-    console.log('fizz');
-  }
-  else {
-    console.log(i);
-  }
-}
+    for (var i = 1; i < number; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        $('.result').append('<li> fizzbuzz </li>');
+      }
+      else if (i % 5 === 0) {
+        $('.result').append('<li> buzz </li>');
+      }
+      else if (i % 3 === 0) {
+        $('.result').append('<li> fizz </li>');
+      }
+      else {
+        $('.result').append('<li>' + i + '</li>');
+      }
+    }
+  }); // pairs with .formInput on submit
+}); //pairs with document ready function
